b94e14a328c82f7cc1e17e4504ca1fa27d3af47d
linfeng <1029776125@qq.com>
2023-06-01 23:45:23 +0800

    repository init

---
+++ src/main/java/com/tw/leave/LeaveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tw.leave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tw/leave/LeaveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
